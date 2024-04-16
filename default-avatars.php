90e886f0881f6cd2de52fe00c491cba778a1b7e1
Henry Wright <henrywright@gmail.com>
2024-04-16 12:45:15 +0100

    Fix version

--- default-avatars.php
+++ default-avatars.php
@@ -8,7 +8,7 @@
  * Plugin Name: Default Avatars
  * Plugin URI: https://github.com/toucah/default-avatars
  * Description: Fun default avatars for your WordPress site.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Requires at least: 6.4
  * Requires PHP: 8.1
  * Author: toucah
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: henry.wright
 Tags: users, avatars
 Requires at least: 6.4
 Tested up to: 6.4
-Stable tag: 1.0.0
+Stable tag: 1.0.1
 Requires PHP: 8.1
 License: GPLv3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
